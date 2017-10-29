20f90376225dc1f8fc908fa963cab634319c3ab3
Gary Qian <gqian1@jhu.edu>
2017-10-28 22:13:35 -0400

    Start android (may use twitter later) and more flask

--- src/backend/flaskserver.py
+++ src/backend/flaskserver.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
